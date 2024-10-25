9544251ffa12cf7d549d14e130bc099a30f5d893
zjyezaire <zaire.elleby@gmail.com>
2024-10-25 13:29:25 -0400

    working commit

--- src/WeatherForecast.jsx
+++ src/WeatherForecast.jsx
@@ -1,39 +1,25 @@
-import './WeatherForecast.css'
+import "./WeatherForecast.css";
 
 const WeatherForecast = (props) => {
   return (
-
     <div className="weather">
+      <h2>{props.forecast.day}</h2>
 
-      <h2>
-        {props.forecast.day}
-      </h2>
-
-      <img
-        src={props.forecast.img}
-        alt={props.forecast.imgAlt}
-      />
+      <img src={props.forecast.img} alt={props.forecast.imgAlt} />
 
       <p>
-        <span>
-          conditions:
-        </span>
+        <span>conditions:</span>
 
         {props.forecast.conditions}
-
       </p>
 
       <p>
-        <span>
-          time:
-        </span>
+        <span>time:</span>
 
         {props.forecast.time}
-        
       </p>
-
     </div>
   );
-}; 
+};
 
 export default WeatherForecast;
